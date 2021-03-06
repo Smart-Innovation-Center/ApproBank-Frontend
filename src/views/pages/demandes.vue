9ca59530a67de99dfc4867624bde092170e17690
OriHenoc <orihenoc@gmail.com>
2021-03-06 10:54:17 +0000

    update router link

--- src/views/pages/demandes.vue
+++ src/views/pages/demandes.vue
@@ -9,112 +9,112 @@
         <div class="sidebar-wrapper">
           <ul class="nav text-left">
             <li class="nav-item">
-              <router-link  href="dashboard">
+              <router-link  to="dashboard">
                 <i class="material-icons">dashboard</i>
                 <p>Tableau de Bord</p>
-              </a>
+              </router-link>
             </li>
             <li class="nav-item ">
-              <router-link  href="profile">
+              <router-link  to="profile">
                 <i class="material-icons">person</i>
                 <p>Profil</p>
-              </a>
+              </router-link>
             </li>
             <li class="nav-item" v-if="userInfos.roles[0].slug==='structureOM'">
-              <router-link  href="mesAgences">
+              <router-link  to="mesAgences">
                 <i class="material-icons">store_mall_directory</i>
                 <p>Mes Agences</p>
-              </a>
+              </router-link>
             </li>
             <li class="nav-item active">
                     <a
                       class="nav-link collapsed text-truncate"
-                      href="#sousmen"
+                      to="#sousmen"
                       data-toggle="collapse"
                       data-target="#sousmen"
                     >
                       <i class="material-icons">account_balance_wallet</i>
                       <p>Approvisionnements</p>
-                    </a>
+                    </router-link>
                     <div class="collapse" id="sousmen" aria-expanded="false">
                       <ul class="flex-column pl-2 nav">
                        <li class="nav-item" v-if="userInfos.roles[0].slug==='structureOM'">
-                          <router-link  href="supply">
+                          <router-link  to="supply">
                             <i class="material-icons">add_circle</i>
                             <p>Nouvelle Demande</p>
-                          </a>
+                          </router-link>
                         </li>
                       <li class="nav-item active" v-if="userInfos.roles[0].slug==='adminBanque' || userInfos.roles[0].slug==='validatorBanque' || userInfos.roles[0].slug==='validatorOMCI'">
-                          <router-link  href="demandes">
+                          <router-link  to="demandes">
                             <i class="material-icons">schedule</i>
                             <p>Demandes en attente</p>
-                          </a>
+                          </router-link>
                         </li>
                         <li class="nav-item">
-                          <router-link  href="listeDemandes">
+                          <router-link  to="listeDemandes">
                             <i class="material-icons">rule</i>
                             <p>Liste des demandes</p>
-                          </a>
+                          </router-link>
                         </li>
                       </ul>
                     </div>
                   </li>
             <li class="nav-item" v-if="userInfos.roles[0].slug!=='superAdmin' && userInfos.roles[0].slug!=='supervisor'">
-              <router-link  href="notifications">
+              <router-link  to="notifications">
                 <i class="material-icons">notifications</i>
                 <p>Notifications</p>
-              </a>
+              </router-link>
             </li>
             <li class="nav-item" v-if="userInfos.roles[0].slug==='superAdmin' || userInfos.roles[0].slug==='validatorOMCI'">
               <a
                 class="nav-link collapsed text-truncate"
-                href="#sousmenuAdmin"
+                to="#sousmenuAdmin"
                 data-toggle="collapse"
                 data-target="#sousmenuAdmin"
               >
                 <i class="material-icons">visibility</i>
                 <p>Administration</p>
-              </a>
+              </router-link>
               <div class="collapse" id="sousmenuAdmin" aria-expanded="false">
                 <ul class="flex-column pl-2 nav">
                   <li class="nav-item" v-if="userInfos.roles[0].slug==='managerOMCI'">
-                    <router-link  href="validateursOM">
+                    <router-link  to="validateursOM">
                       <i class="material-icons">done_all</i>
                       <p>Validateurs</p>
-                    </a>
+                    </router-link>
                   </li>
                   <li class="nav-item" v-if="userInfos.roles[0].slug==='validatorOMCI' || userInfos.roles[0].slug==='managerOMCI'">
               <a
                 class="nav-link collapsed text-truncate"
-                href="#sousmenuStructure"
+                to="#sousmenuStructure"
                 data-toggle="collapse"
                 data-target="#sousmenuStructure"
               >
                 <i class="material-icons">home</i>
                 <p>Gestion des Structures</p>
-              </a>
+              </router-link>
               <div class="collapse" id="sousmenuStructure" aria-expanded="false">
                 <ul class="flex-column pl-2 nav">
                   <li class="nav-item">
-                    <router-link  href="#">
+                    <router-link  to="#">
                       <i class="material-icons">palette</i>
                       <p>Liste des Structures</p>
-                    </a>
+                    </router-link>
                   </li>
                   <li class="nav-item">
-                    <router-link  href="#">
+                    <router-link  to="#">
                       <i class="material-icons">palette</i>
                       <p>Liste des Agences</p>
-                    </a>
+                    </router-link>
                   </li>
                 </ul>
               </div>
             </li>
             <li class="nav-item" v-if="userInfos.roles[0].slug==='validatorOMCI'">
-                    <router-link  href="comptesBanqueOrange">
+                    <router-link  to="comptesBanqueOrange">
                       <i class="material-icons">account_balance</i>
                       <p>Comptes Banque Orange</p>
-                    </a>
+                    </router-link>
                   </li>
                 </ul>
               </div>
@@ -122,54 +122,54 @@
             <li class="nav-item">
               <a
                 class="nav-link collapsed text-truncate"
-                href="#sousmenu"
+                to="#sousmenu"
                 data-toggle="collapse"
                 data-target="#sousmenu"
               >
                 <i class="material-icons">settings</i>
                 <p>Paramètres</p>
-              </a>
+              </router-link>
               <div class="collapse" id="sousmenu" aria-expanded="false">
                 <ul class="flex-column pl-2 nav">
                 <li class="nav-item" v-if="userInfos.roles[0].slug==='superAdmin'">
-                    <router-link  href="#">
+                    <router-link  to="#">
                       <i class="material-icons">info_outline</i>
                       <p>Approbank</p>
-                    </a>
+                    </router-link>
                   </li>
                 <li class="nav-item">
-                    <router-link  href="#">
+                    <router-link  to="#">
                       <i class="material-icons">palette</i>
                       <p>Affichage</p>
-                    </a>
+                    </router-link>
                   </li>
                   <li class="nav-item" v-if="userInfos.roles[0].slug==='superAdmin'">
-                    <router-link  href="roles">
+                    <router-link  to="roles">
                       <i class="material-icons">safety_divider</i>
                       <p>Gestion des Rôles</p>
-                    </a>
+                    </router-link>
                   </li>
                   <li class="nav-item" v-if="userInfos.roles[0].slug==='superAdmin'">
-                    <router-link  href="#">
+                    <router-link  to="#">
                       <i class="material-icons">lock</i>
                       <p>Gestion des Permissions</p>
-                    </a>
+                    </router-link>
                   </li>
                 </ul>
               </div>
             </li>
             <hr class="bg-orange"/>
             <li class="nav-item">
-              <router-link  href="#">
+              <router-link  to="#">
                 <i class="material-icons">menu_book</i>
                 <p>Documentation</p>
-              </a>
+              </router-link>
             </li>
             <li class="nav-item">
-              <router-link  href="#">
+              <router-link  to="#">
                 <i class="material-icons">support_agent</i>
                 <p>Service Support</p>
-              </a>
+              </router-link>
             </li>
           </ul>
         </div>
@@ -181,7 +181,7 @@
         >
           <div class="container-fluid">
             <div class="navbar-wrapper">
-              <a class="navbar-brand" href="javascript:;"
+              <a class="navbar-brand" to="javascript:;"
                 >Demandes d'approvisionnement en attente d'approbation</a
               >
             </div>
@@ -201,12 +201,12 @@
             <div class="collapse navbar-collapse justify-content-end">
               <ul class="navbar-nav">
                 <li class="nav-item">
-                  <router-link  href="javascript:;">
+                  <router-link  to="javascript:;">
                     <i class="material-icons">dashboard</i>
                     <p class="d-lg-none d-md-block">
                       Tableau de bord
                     </p>
-                  </a>
+                  </router-link>
                 </li>
                 <li class="nav-item dropdown" v-if="userInfos.roles[0].slug!=='superAdmin'">
                   <a
